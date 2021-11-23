ca0e1f79d2465856b848b510b28e3944287afc99
AbhimanyuShetty <69106225+AbhimanyuShetty@users.noreply.github.com>
2021-11-23 09:10:58 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCc6VjOFfRYj3OXBQY2_TiYa8fquoO2vkk",
+  authDomain: "c71pro.firebaseapp.com",
+  projectId: "c71pro",
+  storageBucket: "c71pro.appspot.com",
+  messagingSenderId: "916748790301",
+  appId: "1:916748790301:web:3e5bf6c1263ade226774f4"
+};
